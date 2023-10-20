759201aabde8dcaabfc530e95c5bc01b764b8ca5
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2023-10-20 05:52:25 +0530

    here

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <raylib.h>
+
+/* PLEASE CHOOSE THE C++ RAYLIB COMPILER 
+OR ELSE YOU WILL GET AN ERROR SAYING THAT #INCLUDE<RAYLIB.H> FILE IS NOT FOUND */
+using namespace std;
+
+int main () {
+
+    cout<<"Hello my lovely boy"<<endl;
+    return 0;
+}
